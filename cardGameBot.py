e722a3a6619b42c7e4568e967cf493e0b882b967
Strassboom <collinstrassburg@gmail.com>
2019-06-11 19:45:12 -0400

    Added cardGameBot.py with base skeleton CardDeck class.

---
+++ cardGameBot.py
@@ -0,0 +1,45 @@
+class CardDeck:
+    def __init__(self):
+        self.cards = [
+            [0],
+            [0,0,0,0,0],
+            [0,0],
+            [0,0],
+            [0,0],
+            [0,0],
+            [0],
+            [0],
+            [0],
+            [0]
+        ]
+        self.cardActs = [self.mercenary,self.guard,self.priest,self.baron,self.handmaid,self.prince,self.king,self.countess,self.princess,self.emperor]
+
+    def mercenary(self):
+        pass
+    
+    def guard(self):
+        pass
+
+    def priest(self):
+        pass
+
+    def baron(self):
+        pass
+
+    def handmaid(self):
+        pass
+
+    def prince(self):
+        pass
+
+    def king(self):
+        pass
+
+    def countess(self):
+        pass
+
+    def princess(self):
+        pass
+
+    def emperor(self):
+        pass
